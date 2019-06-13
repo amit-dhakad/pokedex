1117377addbec6a96320cd10b1ae36fa6a07ec8e
Amit Dhakad <amit0502dhakad@gmail.com>
2019-06-13 18:22:58 +0530

    added pokecard and pokedex

--- src/App.js
+++ src/App.js
@@ -1,24 +1,10 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-
+import Pokecard from './components/pokecard/pokecard';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Pokecard name="Charmander" id={4} type="fire" exp={62} />
     </div>
   );
 }
