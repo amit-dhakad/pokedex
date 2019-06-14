7805610c7508772ff62b00879b91d274f3a42bdb
Amit Dhakad <amit0502dhakad@gmail.com>
2019-06-14 12:16:54 +0530

    Updates

---
+++ precache-manifest.b9181bc813273f8b17fc85197ff096e3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2718e9d712b10f3678e1fe55ccdda6c4",
+    "url": "/pokedex/index.html"
+  },
+  {
+    "revision": "515798a703002d1e4fbd",
+    "url": "/pokedex/static/css/main.e973de18.chunk.css"
+  },
+  {
+    "revision": "5694e1898ca4c92fd14d",
+    "url": "/pokedex/static/js/2.6408f43b.chunk.js"
+  },
+  {
+    "revision": "515798a703002d1e4fbd",
+    "url": "/pokedex/static/js/main.81ea57e4.chunk.js"
+  },
+  {
+    "revision": "499fa032a833d26aac94",
+    "url": "/pokedex/static/js/runtime~main.fa119ff4.js"
+  }
+]);
