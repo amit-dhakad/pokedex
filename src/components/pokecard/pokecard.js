439f77c6f2c161b9dba373bf20ad498d7fb7dcae
Amit Dhakad <amit0502dhakad@gmail.com>
2019-06-14 15:21:16 +0530

    changed api url

--- src/components/pokecard/pokecard.js
+++ src/components/pokecard/pokecard.js
@@ -3,7 +3,8 @@ import './pokecard.css';
 /* const POKE_API =
   'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/';
  */
-const POKE_API = `https://assets.pokemon.com/assets/cms2/img/pokedex/detail/`;
+// const POKE_API = `https://assets.pokemon.com/assets/cms2/img/pokedex/detail/`;
+const POKE_API = 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/';
 
 let padToThree = number => (number <= 999 ? `00${number}`.slice(-3) : number);
 class Pokecard extends Component {
